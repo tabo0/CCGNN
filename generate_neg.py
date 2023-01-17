de7df7a701c73f7c0fd4b3fcd71d964bdece6158
tabo0 <48040483+tabo0@users.noreply.github.com>
2023-01-17 20:30:41 +0800

    Update generate_neg.py

--- generate_neg.py
+++ generate_neg.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time : 2021/1/31 11:15
-# @Author : ZM7
-# @File : generate_neg
-# @Software: PyCharm
 import pandas as pd
 import pickle
 from utils import myFloder, pickle_loader, collate, trans_to_cuda, eval_metric, collate_test, user_neg
